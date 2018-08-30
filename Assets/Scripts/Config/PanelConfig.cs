a3ee28e00f9be29dfb67e61e69425db75361cb72
Waldemar Tomme <waldetomme@gmail.com>
2018-08-30 19:04:04 +0200

    Added default values

--- Assets/Scripts/Config/PanelConfig.cs
+++ Assets/Scripts/Config/PanelConfig.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class PanelConfig : SubConfig
 {
+    public PanelConfig()
+    {
+        ColorP = UnityEngine.Color.white;
+        ShadowP = UnityEngine.Color.black;
+        Opacity = 0.1f;
+        DashboardOpacity = 1f;
+        FontSize = 30;
+        Format = "{0}: {1}";
+    }
+
     public string Color { get { return Get(); } set { Set(value); } }
 
     [IniSerialization(true)]
--- Assets/Scripts/Config/ServerConfig.cs
+++ Assets/Scripts/Config/ServerConfig.cs
@@ -3,6 +3,12 @@ using MadMilkman.Ini;
 
 public class ServerConfig
 {
+    public ServerConfig()
+    {
+        Binds = new string[] { "localhost", "127.0.0.1" };
+        Port = 45689;
+    }
+
     public string[] Binds { get; set; }
 
     public int Port { get; set; }
